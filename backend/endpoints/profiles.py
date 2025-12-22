55b80dc69c99e378b755e2393d0e60fa344c359a
fisunoff <fisunoff@yandex.ru>
2025-12-22 21:00:02 +0300

    редактирование информации о себе

--- backend/endpoints/profiles.py
+++ backend/endpoints/profiles.py
@@ -1,11 +1,11 @@
 from typing import Sequence
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from models import Profile, get_db
-from schemas.profile import ProfileReadData
+from schemas.profile import ProfileReadData, BaseProfileData
 from utils.jwt_token import verify_admin_role, verify_token
 
 profile_router = APIRouter(
@@ -39,3 +39,25 @@ async def get_me(
     stmt = select(Profile).where(Profile.id == pk)
     result = (await db.execute(stmt)).scalars().first()
     return result
+
+
+@profile_router.put('/me', response_model=ProfileReadData)
+async def update_me(
+        data: BaseProfileData,
+        db: AsyncSession = Depends(get_db),
+        token_payload: dict = Depends(verify_token),
+):
+    pk = int(token_payload['sub'])
+    stmt = select(Profile).where(Profile.id == pk)
+    profile = (await db.execute(stmt)).scalars().first()
+    if not profile:
+        raise HTTPException(status_code=404, detail='Profile not found')
+    profile.name = data.name
+    profile.surname = data.surname
+    profile.patronymic = data.patronymic
+    profile.bank_number = data.bank_number
+    profile.phone_number = data.phone_number
+    db.add(profile)
+    await db.commit()
+    await db.refresh(profile)
+    return profile
